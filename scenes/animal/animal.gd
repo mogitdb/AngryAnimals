6ccf025a74bb1fa74b523ee6906aee8adcfa9d0b
Mecha Ophidian <152424633+mogitdb@users.noreply.github.com>
2024-07-12 11:25:22 -0400

    Dragging Action
    +Added Drag constraints
    +Display Drag co-ordinates (debug)

--- scenes/animal/animal.gd
+++ scenes/animal/animal.gd
@@ -4,6 +4,15 @@ extends RigidBody2D
 enum ANIMAL_STATE { READY, DRAG, RELEASE }
 
 
+const DRAG_LIM_MAX: Vector2 = Vector2(0,60)
+const DRAG_LIM_MIN: Vector2 = Vector2(-60, 0)
+
+
+var _start: Vector2 = Vector2.ZERO
+var _drag_start: Vector2 = Vector2.ZERO
+var _dragged_vector: Vector2 = Vector2.ZERO
+
+
 @onready var label = $Label
 
 
@@ -12,13 +21,14 @@ var _state: ANIMAL_STATE = ANIMAL_STATE.READY
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	_start = position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	update(delta)
-	label.text = "%s" % ANIMAL_STATE.keys()[_state]
+	label.text = "%s\n" % ANIMAL_STATE.keys()[_state]
+	label.text += "%.1f, %.1f" % [_dragged_vector.x, _dragged_vector.y]
 
 
 func set_new_state(new_state: ANIMAL_STATE) -> void:
@@ -26,7 +36,7 @@ func set_new_state(new_state: ANIMAL_STATE) -> void:
 	if _state == ANIMAL_STATE.RELEASE:
 		freeze = false
 	elif _state == ANIMAL_STATE.DRAG:
-		pass
+		_drag_start = get_global_mouse_position()
 
 
 func detect_release() -> bool:
@@ -37,13 +47,33 @@ func detect_release() -> bool:
 	return false
 
 
+func get_dragged_vector(gmp: Vector2) -> Vector2:
+	return gmp - _drag_start
+
+
+func drag_in_limits() -> void:
+	_dragged_vector.x = clampf(
+		_dragged_vector.x,
+		DRAG_LIM_MIN.x,
+		DRAG_LIM_MAX.x
+	)
+	_dragged_vector.y = clampf(
+		_dragged_vector.y,
+		DRAG_LIM_MIN.y,
+		DRAG_LIM_MAX.y
+	)
+	position = _start + _dragged_vector
+	
+
 func update_drag() -> void:
 	
 	if detect_release() == true:
 		return
 	
 	var gmp = get_global_mouse_position()
-	position = gmp
+	_dragged_vector = get_dragged_vector(gmp)
+	drag_in_limits()
+
 
 func update(delta: float) -> void:
 	match _state:
