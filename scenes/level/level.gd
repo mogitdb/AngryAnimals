2e86150392ed5d253a93b6bdfbe98551903addd6
Mecha Ophidian <152424633+mogitdb@users.noreply.github.com>
2024-07-14 14:38:08 -0400

    Level Selection
    +Levels now selectable
    +Return to menu with Esc key
    +Scale level button on hover

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 const ANIMAL = preload("res://scenes/animal/animal.tscn")
-
+const MAIN = preload("res://scenes/main/main.tscn")
 
 @onready var animal_start = $AnimalStart
 
@@ -15,7 +15,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().change_scene_to_packed(MAIN)
 
 
 func add_animal() -> void:
